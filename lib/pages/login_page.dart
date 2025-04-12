53f7818da088ebb82fe80b185645f2d6c90258a2
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-04-12 12:48:51 +0200

    added image to assets and made app assets file

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,7 +6,12 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          
+        ],
+      ),
     );
   }
 }
